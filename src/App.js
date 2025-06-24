4cba6191d8abd6a2c8e4afdf6469ab0284a8724c
T-force609 <101106126+T-force609@users.noreply.github.com>
2025-06-24 13:32:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
         <div className='App-header'>
           <Routes>
             <Route element={<Layout />} />
-            <Route path='codewithajdev.onrender.com/' element={<Home />} />
+            <Route path='/' element={<Home />} />
               <Route exact path='/project' element={<ProjectCard />} />
               <Route path='projects/:id' element={<ProjectDetail />} />
               <Route exact path='/contact' element={<ContactPage />} />
