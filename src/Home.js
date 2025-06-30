0cf7cc93df1533af34a6010eefc9cfb809827dde
T-force609 <101106126+T-force609@users.noreply.github.com>
2025-06-30 16:02:09 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className="w-full min-h-screen flex flex-col md:flex-row ">
             <div className=" rounded-full overflow-hidden w-[300px] h-[300px] ml-20 mt-[100px] mr-20 border-[10px] border-red-300">
-                <img src={ImageIcon} alt="icon"/>
+                <img src={ImageIcon} alt="icon" className="w-full h-full"/>
             </div>
             <div className="flex w-2/3 h-2/3 pt-20 justify-center ">
                 <div className='flex flex-row w-[50%] h-[120px] justify-center '>
