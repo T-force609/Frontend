c035610bffe8ad44cbd95cdfcd2ae6d69556c4f4
T-force609 <101106126+T-force609@users.noreply.github.com>
2025-07-03 15:06:11 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,10 @@ const Home = () => {
 
     return (
         <div className="w-full min-h-screen flex flex-col md:flex-row ">
-            <div className=" rounded-full overflow-hidden w-[200px] h-[200px] ml-20 mt-[100px] mr-20 border-[10px] border-red-300 sm:w-[300px] h-[300px]">
+            <div className=" rounded-full overflow-hidden w-[200px] h-[150px] ml-20 mt-[100px] mr-20 border-[10px] border-red-300 sm:w-[300px] h-[300px]">
                 <img src={ImageIcon} alt="icon" className="w-full h-full"/>
             </div>
-            <div className="flex w-2/3 h-2/3 pt-20 justify-center ">
+            <div className="block w-full h-full md:flex w-2/3 h-2/3 pt-20 justify-center ">
                 <div className='flex flex-row w-[100%] h-[120px] items-center justify-center sm:w-[100%] mb-[60px] '>
                     <p className="text-gray-200 ml-4 w-auto">I specialize in creating stunning 3D art with Blender, building dynamic web applications
                         using Django and React, and developing intelligent machine learning solutions with Python.
